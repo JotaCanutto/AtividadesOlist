0d77132d038f5658bddddff0da8b32f9dce32efb
Olist <olist@Olists-MacBook-Air.local>
2020-12-08 15:58:14 -0300

    Ajustadas as features e adicionada subcategorias e novos atributos para produto

--- AtividadesOlist/atividade2.py
+++ AtividadesOlist/atividade2.py
@@ -1,6 +1,7 @@
 class Categoria:
     __id : int
     __nome : str
+    __lista_subcategorias : str
 
     def __init__(self, id):
         self.__id = id
@@ -15,13 +16,25 @@ class Categoria:
         if type(nome) == str:
             self.__nome = nome
 
+    def get_lista_subcategorias(self) -> str:
+        return self.__lista_subcategorias
+
+    def set_lista_subcategorias(self, lista_subcategorias):
+        if type(lista_subcategorias) == str:
+            self.__lista_subcategorias = lista_subcategorias
+
 class Produto:
     __id : int
     __nome : str
     __descricao : str
     __categoria : str
+    __subcategoria : str
     __preco : float
     __quantidade : int
+    __peso : float
+    __altura : float
+    __largura : float
+    __comprimento : float
 
     def __init__(self, id):
         self.__id = id
@@ -64,12 +77,40 @@ class Produto:
         if type(quantidade) == int:
             self.__quantidade = quantidade
 
+    def get_peso(self) -> float:
+        return self.__peso
+
+    def set_peso(self, peso):
+        if type(peso) == float:
+            self.__peso = peso
+
+    def get_altura(self) -> float:
+        return self.__altura
+
+    def set_altura(self, altura):
+        if type(altura) == float:
+            self.__altura = altura
+
+    def get_largura(self) -> float:
+        return self.__largura
+
+    def set_largura(self, largura):
+        if type(largura) == float:
+            self.__largura = largura
+
+    def get_comprimento(self) -> float:
+        return self.__comprimento
+
+    def set_comprimento(self, comprimento):
+        if type(comprimento) == float:
+            self.__comprimento = comprimento
+
 lista_de_produtos = []
 lista_de_categorias = []
-cats = ''
+lista_de_subcategorias = []
+cats = []
 product_id = 0
 categoria_id = 0
-subcategoria_id = 0
 print('Cadastro de Produtos\n')
 
 while True:
@@ -80,9 +121,69 @@ while True:
     if opcao == '1':
         produto = Produto(product_id)
         produto.set_nome(input('Nome:'))
-        produto.set_descricao(input('Descrição:'))
-        produto.set_preco(float(input('Preço Unitário:')))
-        produto.set_quantidade(int(input('Quantidade:')))
+
+        while True:
+            desc = input('Descrição:')
+
+            if len(desc) < 20:
+                print('A descrição deve conter pelo menos 20 caracteres')
+            else:
+                produto.set_descricao(desc)
+                break
+
+        while True:
+            preco = float(input('Preço Unitário:'))
+
+            if type(preco) != float:
+                print('Preço inválido, favor digitar novamente')
+            else:
+                produto.set_preco(preco)
+                break
+
+        while True:
+            peso = float(input('Peso Unitário:'))
+
+            if type(peso) != float:
+                print('Peso inválido, favor digitar novamente')
+            else:
+                produto.set_peso(peso)
+                break
+
+        while True:
+            altura = float(input('Altura:'))
+
+            if type(altura) != float:
+                print('Altura inválida, favor digitar novamente')
+            else:
+                produto.set_altura(altura)
+                break
+
+        while True:
+            largura = float(input('Largura:'))
+
+            if type(largura) != float:
+                print('Largura inválida, favor digitar novamente')
+            else:
+                produto.set_largura(largura)
+                break
+
+        while True:
+            comprimento = float(input('Comprimento:'))
+
+            if type(comprimento) != float:
+                print('Comprimento inválido, favor digitar novamente')
+            else:
+                produto.set_comprimento(comprimento)
+                break
+
+        while True:
+            qtde = int(input('Quantidade:'))
+
+            if type(qtde) != int:
+                print('Quantidade inválida, favor digitar novamente')
+            else:
+                produto.set_preco(qtde)
+                break
 
         while True:
             for categorias in lista_de_categorias:
@@ -91,20 +192,28 @@ while True:
             id_categoria = int(input('Selecione o número da categoria:'))
 
             for categoria in lista_de_categorias:
-                if id_categoria == categoria.get_id():
-                    cats += categoria.get_nome() + ', '
+                if int(id_categoria) == categoria.get_id():
+                    cats.append(categoria.get_nome())
 
             opt = input('Adicionar mais Categorias a este produto? <S/N>').lower()
 
             if opt == 'n':
                 break
-        produto.set_categoria(categorias)
-        print(categorias)
+        produto.set_categoria(', '.join(cats))
         lista_de_produtos.append(produto)
         product_id += 1
+        cats = ''
     elif opcao == '2':
         categoria = Categoria(categoria_id)
         categoria.set_nome(input('Nome:'))
+
+        while True:
+            lista_de_subcategorias.append(input('Nome da subcategoria:'))
+            op = input('Adicionar mais subcategorias a esta categoria? <S/N>').lower()
+
+            if op == 'n':
+                break
+        categoria.set_lista_subcategorias(', '.join(lista_de_subcategorias))
         lista_de_categorias.append(categoria)
         categoria_id += 1
     elif opcao == '3':
@@ -113,8 +222,69 @@ while True:
             if alterar == indice.get_id():
                 indice.set_nome(input('Nome:'))
                 indice.set_descricao(input('Descrição:'))
-                indice.set_preco(float(input('Preço Unitário:')))
-                indice.set_quantidade(int(input('Quantidade:')))
+                
+                while True:
+                    desc = input('Descrição:')
+
+                    if len(desc) < 20:
+                        print('A descrição deve conter pelo menos 20 caracteres')
+                    else:
+                        produto.set_descricao(desc)
+                        break
+
+                while True:
+                    preco = float(input('Preço Unitário:'))
+
+                    if type(preco) != float:
+                        print('Preço inválido, favor digitar novamente')
+                    else:
+                        produto.set_preco(preco)
+                        break
+
+                while True:
+                    peso = float(input('Peso Unitário:'))
+
+                    if type(peso) != float:
+                        print('Peso inválido, favor digitar novamente')
+                    else:
+                        produto.set_peso(peso)
+                        break
+
+                while True:
+                    altura = float(input('Altura:'))
+
+                    if type(altura) != float:
+                        print('Altura inválida, favor digitar novamente')
+                    else:
+                        produto.set_altura(altura)
+                        break
+
+                while True:
+                    largura = float(input('Largura:'))
+
+                    if type(largura) != float:
+                        print('Largura inválida, favor digitar novamente')
+                    else:
+                        produto.set_largura(largura)
+                        break
+
+                while True:
+                    comprimento = float(input('Comprimento:'))
+
+                    if type(comprimento) != float:
+                        print('Comprimento inválido, favor digitar novamente')
+                    else:
+                        produto.set_comprimento(comprimento)
+                        break
+
+                while True:
+                    qtde = int(input('Quantidade:'))
+
+                    if type(qtde) != int:
+                        print('Quantidade inválida, favor digitar novamente')
+                    else:
+                        produto.set_preco(qtde)
+                        break
 
                 while True:
                     for categorias in lista_de_categorias:
@@ -137,7 +307,10 @@ while True:
                   ' - Nome: '+indice.get_nome()+
                   ' - Descrição: '+indice.get_descricao()+
                   ' - Preço: '+str(indice.get_preco())+
-                  ' - Quantidade: '+str(indice.get_quantidade())+
+                  ' - Peso: '+str(indice.get_peso())+
+                  ' - Altura: '+str(indice.get_altura())+
+                  ' - Largura: '+str(indice.get_largura())+
+                  ' - Comprimento: '+str(indice.get_comprimento())+
                   ' - Categorias: '+indice.get_categoria())
     elif opcao == '5':
         excluir = int(input('Digite o id do produto que deseja excluir: '))
